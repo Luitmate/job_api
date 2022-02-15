9bbeb7ef8f1f9a1d1ae4296a0552c499c191d478
lunov08 <lukitmate@gmail.com>
2022-02-15 18:38:21 -0300

    database config

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express')
+const sequelize = require('./db/config')
+
 const app = express()
 
 app.use(express.json())
@@ -16,4 +18,10 @@ const PORT = process.env.PORT || 3000
 
 app.listen(PORT, async(req, res) => {
     console.log(`Serving on port ${PORT}`)
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
 })
