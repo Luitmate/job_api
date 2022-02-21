a82c3585e90ef4f5ece25f555495a07ff307049e
lunov08 <lukitmate@gmail.com>
2022-02-21 08:56:52 -0300

    config security packages

--- src/app.js
+++ src/app.js
@@ -3,10 +3,31 @@ const express = require('express')
 const sequelize = require('./db/config')
 require('./db/associations')
 
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+//Security packages
+app.set('trust proxy', 1)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+	  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+)
+
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 
 const authenticateUser = require('./middleware/authenticate')
 
