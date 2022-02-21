5e1fbc621288f4f4480500f682ec548688f09480
lunov08 <lukitmate@gmail.com>
2022-02-21 10:44:10 -0300

    config env variables

--- src/db/config.js
+++ src/db/config.js
@@ -1,28 +1,28 @@
 require('dotenv').config()
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(
-    process.env.DB_URI,{
-        dialect: "postgres",
-        dialectOptions: {
-            ssl: {
-              require: true,
-              rejectUnauthorized: false
-            }
-        }
-    }
-)
+// const sequelize = new Sequelize(
+//     process.env.DB_URI,{
+//         dialect: "postgres",
+//         dialectOptions: {
+//             ssl: {
+//               require: true,
+//               rejectUnauthorized: false
+//             }
+//         }
+//     }
+// )
   
     
 
-// const sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//         host: process.env.DB_HOST,
-//         dialect: process.env.DB_DIALECT
-//     }
-// )
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
+    }
+)
 
 module.exports = sequelize
