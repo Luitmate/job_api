d96bcf9f64e454a375d2b11c3bc19baf9c40e4fe
lunov08 <lukitmate@gmail.com>
2022-02-21 16:57:35 -0300

    fix middleware bug

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -2,14 +2,37 @@ require('dotenv').config()
 const jwt = require('jsonwebtoken');
 const ExpressError = require('../errors/ExpressError');
 
-const authentication = async(req, res, next) => {
-    const authHeader = req.headers.authorization
-    if(!authHeader || !authHeader.startsWith('Bearer')) {
-        throw new ExpressError('Autenticación inválida')
-    }
-    const token = authHeader.split(' ')[1]
+// const authentication = async(req, res, next) => {
+//     const authHeader = req.headers.authorization
+//     if(!authHeader || !authHeader.startsWith('Bearer')) {
+//         throw new ExpressError('Autenticación inválida')
+//     }
+//     const token = authHeader.split(' ')[1]
 
+//     try {
+//         const payload = jwt.verify(token, process.env.JWT_SECRET)
+//         const userId = payload.userId
+//         const createdBy = jwt.decode(token)
+//         if(req.body.userId !== userId) {
+//             throw new ExpressError('Autenticación inválida')
+//         } else {
+//             req.user = { userId: createdBy }
+//             next()
+//         }
+//     } catch(error) {
+//         throw new ExpressError('Autenticación inválida')
+//     }
+// }
+
+// module.exports = authentication
+
+const authentication = async(req, res, next) => {
     try {
+        const authHeader = req.headers.authorization
+        if(!authHeader || !authHeader.startsWith('Bearer')) {
+            throw new ExpressError('Autenticación inválida')
+        }
+        const token = authHeader.split(' ')[1]
         const payload = jwt.verify(token, process.env.JWT_SECRET)
         const userId = payload.userId
         const createdBy = jwt.decode(token)
@@ -19,8 +42,8 @@ const authentication = async(req, res, next) => {
             req.user = { userId: createdBy }
             next()
         }
-    } catch(error) {
-        throw new ExpressError('Autenticación inválida')
+    } catch (error) {
+        next(error)
     }
 }
 
