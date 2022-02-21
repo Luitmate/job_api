2d742dd477a22a247f6094f03ca21bab5c6b36b2
lunov08 <lukitmate@gmail.com>
2022-02-21 10:36:21 -0300

    fix bug config db

--- src/db/config.js
+++ src/db/config.js
@@ -1,21 +1,18 @@
 require('dotenv').config()
 const { Sequelize } = require('sequelize')
 
-// const sequelize = new Sequelize(
-//     process.env.DB_URI,{
-//         dialect: "postgres",
-//         dialectOptions: {
-//             ssl: {
-//               require: true,
-//               rejectUnauthorized: false
-//             }
-//         }
-//     }
-// )
 const sequelize = new Sequelize(
-    process.env.DB_URI
+    process.env.DB_URI,{
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false
+            }
+        }
+    }
 )
-    
+  
     
 
 // const sequelize = new Sequelize(
