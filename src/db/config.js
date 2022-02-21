16cc4923a5bd7aa6427ac485b78ff7439ba05dbe
lunov08 <lukitmate@gmail.com>
2022-02-21 12:15:23 -0300

    ssl fix bug

--- src/db/config.js
+++ src/db/config.js
@@ -1,6 +1,14 @@
 require('dotenv').config()
 const { Sequelize } = require('sequelize')
 
+const sequelize = new Sequelize(
+    process.env.DB_URI,
+    {
+        ssl: { rejectUnauthorized: false }
+        
+    }
+)
+
 // const sequelize = new Sequelize(
 //     process.env.DB_URI,{
 //         dialect: "postgres",
@@ -15,19 +23,19 @@ const { Sequelize } = require('sequelize')
   
     
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT
-    }, {
-        ssl: {
-            rejectUnauthorized: false
-        }
-    }
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         host: process.env.DB_HOST,
+//         dialect: process.env.DB_DIALECT
+//     }, {
+//         ssl: {
+//             rejectUnauthorized: false
+//         }
+//     }
     
-)
+// )
 
 module.exports = sequelize
